b026ba3f46d8477086cc6e660dd745b15423487a
Edgars Skele <edgars.skele@pcc.edu>
2017-02-07 23:05:03 -0800

    print date

--- src/edu/pcc/cis/a4/a4.java
+++ src/edu/pcc/cis/a4/a4.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.a4;
 
+import java.util.Date;
+
 /**
  * Created by Edgars Skele on 2/6/2017.
  */
 public class a4 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
